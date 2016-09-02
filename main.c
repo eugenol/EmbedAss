4f2ac4108558d0046ea2eaea488b4832f650fcfa
eug <eug@DESKTOP-RFODP5D>
2016-09-02 12:21:49 +0200

    Modify setup, add init code for uart, add putch to enable use of printf

--- main.c
+++ main.c
@@ -8,29 +8,43 @@
 //configuration bits will go here
 
 #include <xc.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-//function headers
-void setup(void); //set up is in here
-void mainloop(void);
-
-void main(void) {
-    setup();
-    for(;;)
-        mainloop();
-    return;
+/*
+ * setup routines come here.
+ * e.g setting up the timers, adc, etc.
+ */
+void init_uart(void)
+{
+    SPBRG = 0x19; // 9600 baud @ 4 MHz
+    TXEN = 1; // enable transmitter
+    BRGH = 1; // select high baud rate
+    SPEN = 1; // enable serial port
+    CREN = 1; // enable continuous operation
 }
 
 /*
- * setup should contain all the setup/configuration code
- * such as setting up the timers, adc, etc.
+ * Overwrite putch, so that calling
+ * printf() will print to the USART
  */
-void setup(void) {
-    
+void putch(unsigned char data) {
+    while(!TXIF) //check buffer
+        continue; //wait till ready
+    TXREG  =data; //send data
 }
+
 /*
  * main loop contains the loop that will repeat forever,
  * so all code goes in here.
 */
 void mainloop(void) {
-    
+    printf("Hello, World!\n");
+}
+
+void main(void) {
+    init_uart();
+    for(;;)
+        mainloop();
+    return;
 }
