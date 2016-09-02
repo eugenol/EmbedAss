96df0ab16c96a2b88945b74a7c1bd017289b866c
eug <eug@DESKTOP-RFODP5D>
2016-09-02 11:48:24 +0200

    Added setup and mainloop

--- main.c
+++ main.c
@@ -5,9 +5,32 @@
  * Created on 02 September 2016, 11:28 AM
  */
 
+//configuration bits will go here
 
 #include <xc.h>
 
+//function headers
+void setup(void); //set up is in here
+void mainloop(void);
+
 void main(void) {
+    setup();
+    for(;;)
+        mainloop();
     return;
+}
+
+/*
+ * setup should contain all the setup/configuration code
+ * such as setting up the timers, adc, etc.
+ */
+void setup(void) {
+    
+}
+/*
+ * main loop contains the loop that will repeat forever,
+ * so all code goes in here.
+*/
+void mainloop(void) {
+    
 }
