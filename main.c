91925ece58ea9c68f9dbcd3d7ed235b8a37c1037
eug <eug@DESKTOP-RFODP5D>
2016-09-08 21:52:30 +0200

    add configuration bits

--- main.c
+++ main.c
@@ -5,7 +5,24 @@
  * Created on 02 September 2016, 11:28 AM
  */
 
-//configuration bits will go here
+// PIC16F690 Configuration Bit Settings
+// 'C' source line config statements
+// CONFIG
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA4/OSC2/CLKOUT pin, I/O function on RA5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select bit (MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+/* Using Internal Clock of 4 Mhz */
+#define FOSC 4000000L
 
 #include <xc.h>
 #include <stdio.h>
